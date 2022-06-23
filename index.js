33812e39fe053f4ddf0924080b787863c8505cbd
pitlimitCode <prabow14@gmail.com>
2022-06-23 12:48:15 +0700

    commit on master

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
-const cors = require('cors'); // cors
-
-const userAllRoutes = require("./routes/usersRoutes")
-const recipesRoutes = require("./routes/recipesRoutes")
-const commentsRoutes = require("./routes/commentsRoutes")
-
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors()); 
 
-// FROM ROUTES
+
+const cors = require("cors");
+app.use(cors());
+
+require("dotenv").config();
+// console.log(process.env)
+
+// linter ~ .eslintrc.json
+
+
+// Routes
+const userAllRoutes = require("./routes/usersRoutes");
+const recipesRoutes = require("./routes/recipesRoutes");
+const commentsRoutes = require("./routes/commentsRoutes");
 app.use("/", userAllRoutes);
 app.use("/", recipesRoutes);
 app.use("/", commentsRoutes);
 
-// LISTEN END
-app.listen(port, () => console.log(`Success nodemon from port: '${port}'`) );
+// LAST LISTEN
+app.listen(port, () => console.log(`[nodemen] success, port: '${port}'.`));
